0e2283b96f8a33695c7d160a5efb9f9ecf5d436e
Rayan Kamdem <rayankdm@umich.edu>
2025-06-04 15:14:14 -0500

    Time: 99 ms (68.76%), Space: 28.4 MB (67.51%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -3,7 +3,7 @@ class Solution:
         maxAmt = 0
         currAmt = 0
         l, r = 0, len(heights) - 1
-        while l < r and l < len(heights) and r > 0:
+        while l < r:
             currAmt = (r - l) * min(heights[l], heights[r])
             maxAmt = max(currAmt, maxAmt)
             if heights[l] > heights[r]:
