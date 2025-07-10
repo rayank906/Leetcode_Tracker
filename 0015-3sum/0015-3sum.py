ccd48d916fbc199447b56c1f9f1a5b55139c3ad9
Rayan Kamdem <rayankdm@umich.edu>
2025-07-10 17:06:48 -0400

    Time: 2535 ms (5.01%), Space: 20.2 MB (96.67%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,31 +1,33 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        res = []
+        res = set()
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                if nums[l] + nums[r] < -nums[i]:
-                    l += 1
-                elif nums[l] + nums[r] > -nums[i]:
-                    r -= 1
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                if nums[i] + nums[j] < -nums[k]:
+                    j += 1
+                elif nums[i] + nums[j] > -nums[k]:
+                    k -= 1
                 else:
-                    res.append([nums[i], nums[l], nums[r]])
-                    l += 1
-                    while nums[l] == nums[l - 1] and l < r:
-                        l += 1
-        return res
-
-
-
-        
+                    res.add((nums[i], nums[j], nums[k]))
+                    j += 1
+        return list(res)
         
 
 """
-    1. for every i, use 2sum on the remaining array to find pair
-    of values that equal -nums[i]
-    2. when found, append in res
+   0. res a set
+   0b. sort input arr
+   1. for every element in input arr
+   2. perform a two sum operation to find the other two numbers
+    a. l to idx + 1, r to last element
+    b. if sum(nums[l], nums[r]) < -nums[idx]:
+        l += 1
+    c. if sum > -nums[idx], r -= 1
+    d. else, add nums[i], j, k to res
+    e. return list(res)
+
+    TC: O(n^2), SC: O(n) 
 """
         
