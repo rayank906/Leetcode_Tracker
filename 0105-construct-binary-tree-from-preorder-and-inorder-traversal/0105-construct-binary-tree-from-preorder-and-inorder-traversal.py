d610d302c1fce55995a5df560a9b22724c3a1f28
Rayan Kamdem <rayankdm@umich.edu>
2025-07-25 17:03:18 -0500

    Time: 371 ms (5.02%), Space: 378.9 MB (6.26%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -7,37 +7,42 @@
 
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder or not inorder:
-            return None
-        
-        root = TreeNode(preorder[0])
 
-        if len(preorder) == 1:
+        indexes = defaultdict(int)
+        for i in range(len(inorder)):
+            indexes[inorder[i]] = i
+
+        def dfs(preorder, inorder, indexes):
+            if not preorder or not inorder:
+                return None
+            
+            root = TreeNode(preorder[0])
+
+            if len(preorder) == 1:
+                return root
+            
+            idx = indexes[root.val]
+            
+            root.left = self.buildTree(preorder[1:idx + 1], inorder[:idx])
+            root.right = self.buildTree(preorder[idx + 1:], inorder[idx + 1:])
             return root
         
-        idx = 0
-        for i in range(len(inorder)):
-            if inorder[i] == root.val:
-                idx = i
-                break
-        
-        root.left = self.buildTree(preorder[1:idx + 1], inorder[:idx])
-        root.right = self.buildTree(preorder[idx + 1:], inorder[idx + 1:])
-
-        return root
+        return dfs(preorder, inorder, indexes)
         
 
 
 """
     Base case: array of size 0, return None
     array of size 1, make root and return root
+    0. to avoid looping when searching for index in inorder, make a hashmap of idx
+    and values for inorder
     1. root of binary tree is first element of preorder
         root = TreeNode(preorder[0])
-    2. loop through inorder to find root.
+    2. access hashmap for idx of root.val in inorder
     3. assign root.left to inorder[:i], preorder(1:i + 1)
     4. assign root.right to inorder[i + 1:], preorder(i + 1:)
 
-    TimeC: O(n^2) looping through preorder once
+    TimeC: O(n) looping through preorder once, hashmap for O(1) access
     SpaceC: O(h) for the recursive call stack
 """
         
