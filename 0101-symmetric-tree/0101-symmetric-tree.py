3aeae92a8110767e74522376040bbf7b95b73ce0
Rayan Kamdem <rayankdm@umich.edu>
2025-08-03 13:19:40 -0500

    Time: 3 ms (7.9%), Space: 17.8 MB (54.57%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -8,6 +8,7 @@ class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         if not root:
             return True
+        
         def check(root1, root2):
             if (not root1 and root2) or (not root2 and root1):
                 return False
@@ -23,13 +24,17 @@ class Solution:
         
 
 """
-    1. Base cases: if not root, if not root.left and not root.right, return True
-    2. preorder traversal of left subtree (node, left, right)
-    3. preorder of right subtree (node, right, left)
-    4. return array1 == array2
+    1. Base cases: if not root, return True
+    2. Make a check helper function that takes in two roots
+    these would be the opposite subtree nodes.
+        a. if they're null return True
+        b. if one is null or they're unequal, return False
+        c. else, recursively check the left subtree of root1 and right subtree of root2
+        and right subtree of root1 and left subtree of root2
+    3. call check on left and right subtree
 
     TimeC: O(n)
-    SpaceC: O(n)
+    SpaceC: O(h)
 """
 
         
