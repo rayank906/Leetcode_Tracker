b889cc8cfe53b37073c6d4b13f583fc458c1122d
Rayan Kamdem <rayankdm@umich.edu>
2025-08-06 13:54:43 -0500

    Time: 11 ms (57.74%), Space: 17.8 MB (61.64%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,39 +1,38 @@
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        combs, curSet = [], []
-        total = 0
-        def helper(i, curSet, total):
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+        def helper(i, cur, total):
             if total == target:
-                combs.append(curSet[:])
+                res.append(cur[:])
                 return
-            if total > target or i >= len(candidates):
+            if total > target or i >= len(nums):
                 return
             
-            # append candidates[i]
-            curSet.append(candidates[i])
-            total += candidates[i]
-            helper(i, curSet, total)
+            cur.append(nums[i])
+            total += nums[i]
+            helper(i, cur, total)
+            cur.pop()
+            total -= nums[i]
 
-            curSet.pop()
-            total -= candidates[i]
-
-            # explore without candidates[i]
-            helper(i + 1, curSet, total)
+            helper(i + 1, cur, total)
         
-        helper(0, curSet, total)
-        return combs
+        helper(0, [], 0)
+        return res
 
 """
-    1. create a res array, total variable, currSet array
-    2. call our helper with nums, currSet, res, total
-        a. if total == target, append copy of curSet to res and return
-        b. if total > target, return
-        c. append nums[i], explore all the possibilities with that value
-        by recursively calling helper with i
-        d. pop the nums, explore all possibilities without that value
-        by recursively calling helper with i + 1
-    3. return combs
+    1. make a res array
+    2. make helper, [i, cur, total]
+        a. Base cases
+            i. if total == target, append to res and return
+            ii. if total > target or i >= len(nums), return
+        b. append element at i to cur
+        c. call helper with i to explore all combs with elem i
+        d. pop elem i
+        e. call helper with i + 1 to explore all combs without element i
+    3. call helper
+    4. return res
 
-    TimeC: O(2^t)
-    SpaceC: O(2^t)
+    TimeC: O(2^t/m)
+    SpaceC: O(t/m)
 """
+        
