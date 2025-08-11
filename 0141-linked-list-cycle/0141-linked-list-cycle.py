a8caf35012a01831313e7f10262796695f230485
Rayan Kamdem <rayankdm@umich.edu>
2025-08-11 17:18:15 -0500

    Time: 37 ms (97.19%), Space: 19.9 MB (29.22%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -1,30 +1,26 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        if head is None or head.next is None:
-            return False
-        p1, p2 = head, head
-        while p2 and p2.next:
-            p1 = p1.next
-            p2 = p2.next.next
-            if p1 == p2:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
                 return True
         return False
-        
 
 """
-    1. make two pointers to the start
-    2. p1 moves 1 at a time, p2 moves at twice the speed
-    3. if p1 and p2 meet before p1 reaches nullptr, there exists a cycle
-    4. elif p1 reaches -1, there is no cycle
-
-    Edge cases:
-        if single node, check if next is nullptr, return false if it is
-        if while loop exited, no cycle exists
+    1. init fast, slow to the head
+    2. while fast and fast.next
+    3. slow to the next node, fast to next.next
+    4. if fast ever meets the slow, return True
+    5. if the loop exits, return False
 """
         
