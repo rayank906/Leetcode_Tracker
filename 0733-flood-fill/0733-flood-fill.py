75fcd0e41725214e71f4c543d9c330cedb1a7d26
Rayan Kamdem <rayankdm@umich.edu>
2025-10-31 19:00:54 -0400

    Time: 3 ms (17.23%), Space: 18.1 MB (27.36%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,38 +1,29 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        if not image or image[sr][sc] == color:
-            return image
-        
-        rows, cols = len(image), len(image[0])
+        """
+            1. start at sr, sc, save original color
+            2. check all adjacent neighbours and modify
+            3. recursively check all neighbouring
+        """
+        ROWS, COLS = len(image), len(image[0])
+        orig = image[sr][sc]
         visit = set()
 
-        def dfs(r, c, original):
-            if (r >= rows or c >= cols or 
-                r < 0 or c < 0 or
-                (r, c) in visit or
-                image[r][c] != original):
+        def dfs(r, c):
+            if r >= ROWS or c >= COLS or r < 0 or c < 0 or (r, c) in visit or image[r][c] != orig:
                 return
-            
+
             image[r][c] = color
             visit.add((r, c))
-            dfs(r + 1, c, original)
-            dfs(r - 1, c, original)
-            dfs(r, c + 1, original)
-            dfs(r, c - 1, original)
+            
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
         
-        original = image[sr][sc]
-        dfs(sr, sc, original)
+        dfs(sr, sc)
 
         return image
+            
 
-"""
-    1. go to sr, sc, if color == curr color, return image
-    2. save the orig color, change to color
-    3. dfs helper on r+1, c+1, c-1, r-1 with original
-    DFS Helper
-        a. if r,c oob or r,c in visit return
-        b. if curr color != orig, return
-        c. change its color, dfs on all adjacent nodes
-    4. return image
-"""
         
