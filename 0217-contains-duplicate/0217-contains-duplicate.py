1de4c0c8dfe6b37fc29af45ac2dca834fefa9dd3
Rayan Kamdem <rayankdm@umich.edu>
2025-12-25 09:00:34 -0600

    Time: 14 ms (57.86%), Space: 28.8 MB (91.67%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,19 +1,14 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        set_elements = set(nums)
-        return (len(nums) != len(set_elements))
-
-"""
-    bfa: check every element against all next elements
-    1. for each element, loop through next elements
-    2. return true if element is found again
-    3. return false otherwise
-
-    hash-map/list: check every element against hashmap of prev elements
-    1. loop and check each element against list of prev elements
-    2. if found, return true
-    3. return false otw
-
-    set: make a set of nums. if they are the same length, then list is distinct, else list contains duplicates
-"""
-        
+        """
+            1. loop through nums
+            2. add elements to set
+            3. if seen, return true
+            4. return false
+        """
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False  
