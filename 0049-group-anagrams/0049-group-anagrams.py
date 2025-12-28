956dde37e308827e4ab494284eddd5b4592b9826
Rayan Kamdem <rayankdm@umich.edu>
2025-12-28 09:25:35 -0600

    Time: 2361 ms (5%), Space: 21.6 MB (36.95%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,34 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
+        """
+            1. build freqMap arr, visited bool array
+            2. loop freqMap arr
+            3. for every non visited freqMap,
+                4. check if equal with curr, 
+                b. append to a temp and mark visited
+                5. append temp to result arr
+                6. empty temp
+            O(n^2 * m)
+        """
+        visited = [False] * len(strs)
+        maps = []
+        res= []
         for s in strs:
-            count = [0] * 26
+            freqMap = defaultdict(int)
             for c in s:
-                count[ord(c) - ord('a')] += 1
-            res[tuple(count)].append(s)
-        return list(res.values())
+                freqMap[c] += 1
+            maps.append(freqMap)
+        
+        for i in range(len(maps)):
+            if visited[i]:
+                continue
+            visited[i] = True
+            temp = [strs[i]]
+            for j in range(i + 1, len(maps)):
+                if not visited[j]:
+                    if maps[i] == maps[j]:
+                        temp.append(strs[j])
+                        visited[j] = True
+            res.append(temp)
+        return res
+        
