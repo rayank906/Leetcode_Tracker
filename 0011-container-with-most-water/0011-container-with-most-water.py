db6bade0238a76ef0cf2bfc8ebb9d514ea2dcd08
Rayan Kamdem <rayankdm@umich.edu>
2026-01-03 20:11:56 -0600

    Time: 115 ms (12.88%), Space: 27.7 MB (85.17%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,13 +1,20 @@
 class Solution:
     def maxArea(self, heights: List[int]) -> int:
-        maxAmt = 0
-        currAmt = 0
+        """
+            amt = smallest height * (idx b - idx a)
+            1. use two ptrs to the front and back of arr
+            2. calc amt and store in max
+            3. move ptr with smallest value inwards
+            4. return max
+        """
         l, r = 0, len(heights) - 1
+        area = 0
         while l < r:
-            currAmt = (r - l) * min(heights[l], heights[r])
-            maxAmt = max(currAmt, maxAmt)
-            if heights[l] > heights[r]:
-                r -= 1
-            else:
+            amt = (r - l) * min(heights[l], heights[r])
+            area = max(amt, area)
+            if heights[l] < heights[r]:
                 l += 1
-        return maxAmt
+            else:
+                r -= 1
+        return area
+        
