32e3dc22f55875cada4c43757f88a9e1b4457b55
Rayan Kamdem <rayankdm@umich.edu>
2026-01-05 06:38:09 -0600

    Time: 189 ms (20.55%), Space: 17.8 MB (77.38%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,39 +1,24 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        count = {}
-        res = 0
+        """
+            1. init freqMap
+            2. init l, r, longest to 0
+            3. for given window,
+            4. if r - l + 1 - maxcount <= k (num reps to be made)
+            5. window valid, update longest
+            6. else, slide l while r - l + 1 > k
+            7. return longest
+        """
+        rep = k
+        l, r, longest = 0, 0, 0
+        freqMap = defaultdict(int)
         
-        l = 0
-        maxf = 0
-        for r in range(len(s)):
-            count[s[r]] = 1 + count.get(s[r], 0)
-            maxf = max(maxf, count[s[r]])
-
-            while (r - l + 1) - maxf > k:
-                count[s[l]] -= 1
+        while r < len(s):
+            freqMap[s[r]] += 1
+            while (r - l + 1) - max(freqMap.values()) > k:
+                freqMap[s[l]] -= 1
                 l += 1
-            res = max(res, r - l + 1)
-
-        return res
-
-    
-"""
-    1. find the curr longest substring [with its indexes]
-        a. use a char
-        b. keep track of curr len, max len, maxl, maxr
-        c. init l to 0, loop through s with r
-        d. if s[r] != char, char = s[r], l = r, max[s] update, currlen = 0
-        e. curr len += 1
-    2. using k, figure out by how much we can extend s
-        a. since we have indexes, check if left of maxL and right of maxR are valid
-        b. if both are, we can extend by 2
-        c. if only 1 is, we can extend by 1, else no extend
-    3. return len(curr longest) + extend amount
-
-    TC, SC: O(n), O(1)
-
-    EC:
-        single char string: maxLen will be 1, extend will be 0, returns 1
-        empty char string: loop never runs, extend and maxLen stay 0 so 0 returns
-"""
+            longest = max(longest, r - l + 1)
+            r += 1
+        return longest
         
