bfe37753e6a7223ee193a262d7fb97fc8018aeb1
Rayan Kamdem <rayankdm@umich.edu>
2026-01-19 15:21:08 -0500

    Time: 50 ms (20.56%), Space: 20.6 MB (5.47%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -4,45 +4,33 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-class Solution:   
+class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        def sameTree(root, subroot):
-            if not root and not subroot:
+        """
+            1. make a same tree helper
+            2. base cases
+                a. if not subroot, return true
+                b. if not root, return false
+                a. if same tree, return true
+                b. return subtree(root.left, sub) or subtree(root.right, sub)
+            3. sameTree()
+                if both null, return true
+                if one null, return false
+                if val not equal, return false
+                return sametree(left, left) and sameTree(right, right)
+        """
+        def sameTree(p, q):
+            if not p and not q:
                 return True
-            if not root or not subroot:
+            if not p or not q or p.val != q.val:
                 return False
-            return (root.val == subroot.val) and sameTree(root.left, subroot.left) and sameTree(root.right, subroot.right)
-        
-        if not root and not subRoot:
+            return sameTree(p.left, q.left) and sameTree(p.right, q.right)
+
+        if not subRoot:
             return True
         if not root:
             return False
-        if not subRoot:
+        if sameTree(root, subRoot):
             return True
-        
-        if root.val == subRoot.val:
-            isSameTree = sameTree(root, subRoot)
-            if isSameTree:
-                return True
-        
         return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
-        
-
-"""
-    1. Base cases: if root null, return True
-        if root or subroot null and other not null, return False
-    2. For every node, compare if they are the same trees
-    3. Use a helper function isSametree to compare the nodes
-    4. for every node, check isSameTree on nodes with same value as subRoot
-    5. if isSame returns false, try again recursively
-    6. if isSame returns true, return the node
-
-    TC: O(n * s) where n is root and s is subroot
-    For every node, we're going through s nodes to check for isSameTree
-
-    SC: O(n * s) for recursive call stack
-    For every node, we are creating s stack frames when calling isSameTree
-"""
-
         
