61cc0131cd2bcec4b6c2952c924221c3a76d921e
Rayan Kamdem <rayankdm@umich.edu>
2026-08-02 18:03:45 -0400

    Time: 15 ms (84.3%), Space: 20.5 MB (53.69%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,40 +1,28 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        if not grid:
-            return 0
-        
-        visit = set()
-        rows, cols = len(grid), len(grid[0])
-        maxCount = 0
+        """
+            1. when 1 encountered, count how large island is
+                - use a dfs helper
+                    a. if node oob or visited, return 0
+                    b. visit all neighbors, return count
+            2. as we count, change 1s to 0s
+            3. check if curr count > global count
+            4. return count
+        """
+        ROW, COL = len(grid), len(grid[0])
+        count = 0
 
         def dfs(r, c):
-            if r >= rows or c >= cols or r < 0 or c < 0 or (r, c) in visit or grid[r][c] == 0:
+            if r < 0 or r >= ROW or c < 0 or c >= COL or grid[r][c] == 0:
                 return 0
-            
-            visit.add((r, c))
-            return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
+            grid[r][c] = 0
+            return 1 + dfs(r, c + 1) + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c - 1)
         
-        for row in range(rows):
-            for col in range(cols):
-                if grid[row][col] == 1:
-                    count = dfs(row, col)
-                    maxCount = max(count, maxCount)
+        for r in range(ROW):
+            for c in range(COL):
+                if grid[r][c] == 1:
+                    count = max(dfs(r, c), count)
         
-        return maxCount
+        return count
 
-"""
-    0. make a visit set
-    1. Loop through the grid
-    2. When we encounter a 1, perform a dfs to count the number of 1s
-    in that island
-        DFS helper
-        a. If curr node is 0 or r, c OOB or r,c in visit, return
-        b. add r,c to visit set
-        c. return 1 + dfs of r + 1, r - 1, c + 1, c - 1
-    3. update maxCount of the island with count returned by dfs
-    4. return maxCount
-
-    TimeC: O(m * n)
-    SpaceC: O(m * n)
-"""
         
