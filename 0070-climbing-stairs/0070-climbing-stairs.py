b19a9608d3cf02e8748897e26c31d851fdd98bad
Rayan Kamdem <rayankdm@umich.edu>
2026-08-27 03:25:58 -0400

    Time: 0 ms (100%), Space: 19.2 MB (53.17%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,18 +1,21 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         """
-            - start at 0
-            - climb 1 step or 2 steps
-            - if reach n, return 1
-            - if above n, return 0
-            - cache ways by adding counts
+            - init arr of size 2 = [1, 1]
+            - loop through n - 1 times
+            - calc total = sum of arr values
+            - arr[1] = arr[0]
+            - arr[0] = total
+            - return arr[0]
         """
-        def ways(i, cache):
-            if i == n:
-                return 1
-            if i > n:
-                return 0
-            if i not in cache:
-                cache[i] = ways(i + 1, cache) + ways(i + 2, cache)
-            return cache[i]
-        return ways(0, {})
+        if n <= 1:
+            return 1
+        dp = [1, 1]
+        for i in range(n-1):
+            total = dp[0] + dp[1]
+            dp[1] = dp[0]
+            dp[0] = total
+        return dp[0]
+            
+
+        
